56eb00960f618a5fb0e574a91db66db3ceda03f8
Rafmoc <olejniczak00@gmail.com>
2022-04-21 13:52:01 +0200

    With fibonacci

--- TimeTesting/TimeTesting/Program.cs
+++ TimeTesting/TimeTesting/Program.cs
@@ -8,18 +8,18 @@ namespace TimeTesting
     {
         static void Main(string[] args)
         {
-            TestsHandler(4);
+            TestsHandler(10000);
         }
 
-        static void PrintTime(int timesTested, TimeSpan funcOne, TimeSpan funcOneAv, TimeSpan funcTwo, TimeSpan funcTwoAv)
+        static void PrintTime(int timesTested, TimeSpan funcOne, TimeSpan funcTwo)
         {
             Console.WriteLine("Times Tested = " + timesTested);
             Console.WriteLine("");
             Console.WriteLine("Function 1 Sum = " + funcOne);
             Console.WriteLine("Function 2 Sum = " + funcTwo);
             Console.WriteLine("");
-            Console.WriteLine("Function 1 Average = " + funcOneAv);
-            Console.WriteLine("Function 2 Average = " + funcTwoAv);
+            Console.WriteLine("Function 1 Average = " + new TimeSpan(funcOne.Ticks / timesTested));
+            Console.WriteLine("Function 2 Average = " + new TimeSpan(funcTwo.Ticks / timesTested));
             Console.ReadLine();
         }
 
@@ -33,34 +33,57 @@ namespace TimeTesting
             funcOneWatch.Start();
             for (int i = 0; i < testTimes; i++)
             {
-                FunctionOne();
+                Fib1(24);
             }
             funcOneWatch.Stop();
 
             funcTwoWatch.Start();
             for (int i = 0; i < testTimes; i++)
             {
-                FunctionTwo();
+                Fib2(24);
             }
             funcTwoWatch.Stop();
+
+            Console.WriteLine("Fib1 - " + Fib1(24));
+            Console.WriteLine("Fib2 - " + Fib2(24));
 
             funcOneSum = funcOneWatch.Elapsed;
             funcTwoSum = funcTwoWatch.Elapsed;
 
-            PrintTime(testTimes, funcOneSum, new TimeSpan(funcOneSum.Ticks / testTimes), funcTwoSum, new TimeSpan(funcTwoSum.Ticks / testTimes));
+            PrintTime(testTimes, funcOneSum, funcTwoSum);
         }
 
-        static void FunctionOne()
-        {
-            Thread.Sleep(1000);
-        }
+        //---------------------------------------------------------------------------------------------------------------------------------------------
+        //---------------------------------------------------------------------------------------------------------------------------------------------
+        //---------------------------------------------------------------------------------------------------------------------------------------------
 
-        static void FunctionTwo()
+        public static int Fib1(int n)
         {
-            for(int i = 0; i < 1000; i++)
+            int[] tab = new int[n+1];
+            tab[0] = 0;
+            tab[1] = 1;
+            for (int i = 2; i <= n; i++)
             {
-                Thread.Sleep(1);
+                tab[i] = tab[i - 2] + tab[i - 1];
             }
+            return tab[n];
         }
+
+        public static int Fib2(int n)
+        {
+            if ((n == 0) || (n == 1))
+            {
+                return n;
+            }
+            else
+                return Fib2(n - 1) + Fib2(n - 2);
+        }
+
+        /*static int Fib(int n)
+        {
+            return (n < 2) ? n : Fib(n - 1) + Fib(n - 2);
+        }*/
     }
 }
+
+
